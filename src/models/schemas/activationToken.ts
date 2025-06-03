806af845bcff80ed583d3c4a3c04bcca7c9eb4e6
Arturo Bolivar <ablrcomx@gmail.com>
2025-06-03 18:03:36 -0400

    Fix relative imports

--- src/models/schemas/activationToken.ts
+++ src/models/schemas/activationToken.ts
@@ -1,9 +1,9 @@
 import { pgTable as table, varchar, timestamp, serial, integer } from 'drizzle-orm/pg-core';
-import { users } from '@schemas/users'; // Adjust the path as needed to where your users table is defined
+import { usersSchema } from '@schemas/users'; // Adjust the path as needed to where your users table is defined
 
 export const activationTokens = table('activation_tokens', {
   id: serial('id').primaryKey(),
-  user_id: integer('user_id').notNull().unique().references(() => users.id, { onDelete: 'cascade' }),
+  user_id: integer('user_id').notNull().unique().references(() => usersSchema.id, { onDelete: 'cascade' }),
   token: varchar('token', { length: 255 }).notNull().unique(),
   expires_at: timestamp('expires_at', { withTimezone: true }).notNull(),
   created_at: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
